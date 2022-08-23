0c425f8ad097f032ba2e30dd98c448fbd081232e
Andrew Wang <Andrewruujin@gmail.com>
2022-08-23 18:07:41 -0400

    commit tree c147053f821997c47145ba1428df8c8111b70c3a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(years){
+    const winYears = years.find(r => r.result == "W");
+    if (winYears !== undefined){
+        return winYears.year;
+    }
+    else{
+        return winYears;
+    }
+}
